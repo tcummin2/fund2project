315672911813a2b11866760b10d50dc055451f6a
tpate@nd.edu <tpate@nd.edu@b684008a-aece-c055-49a8-7918b90d006c>
2014-04-07 22:32:08 +0000

    Made Polygon Object

--- trunk/src/main.cpp
+++ trunk/src/main.cpp
@@ -2,13 +2,15 @@
 #include <exception>
 #include <iostream>
 
+using namespace std;
+
 int main() {
     // TODO (Thomas Luppi#1#03/07/14): Add input of parameters and put them in some sort of "Temporary Vector" in the options
     GameEngine eng;
     try{
         eng.go();
     }
-    catch(std::exception &e) {
-        std::cout << e.what();
+    catch(exception &e) {
+        cout << e.what();
     }
 }
