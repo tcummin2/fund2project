f3d0e1e41c5e50090c2c5578b4045d885c1122e9
thomasluppi <thomasluppi@b684008a-aece-c055-49a8-7918b90d006c>
2014-04-11 07:47:59 +0000

    Made more states for climbing ladders

--- trunk/src/Components/Movement/BraveAdventurerMovement.cpp
+++ trunk/src/Components/Movement/BraveAdventurerMovement.cpp
@@ -30,11 +30,11 @@ void BraveAdventurerMovement::go(sf::Time frameTime) {
         b2Vec2 velocity = body->GetLinearVelocity();
 
         if(nextState!=currState) { //State changers
-            if(nextState==MoveState::onLadder) { //Remove gravity and whatnot
+            if((nextState>=MoveState::onLadder && nextState<=MoveState::ladderDown) && !(currState>=MoveState::onLadder && currState<=MoveState::ladderDown)) { //Remove gravity and whatnot
                 physics->getBody()->SetGravityScale(0);
                 physics->getBody()->SetLinearDamping(8.0f);
             }
-            if(currState==MoveState::onLadder) {
+            if(currState>=MoveState::onLadder && currState<=MoveState::ladderDown && !(nextState>=MoveState::onLadder && nextState<=MoveState::ladderDown)) {
                 physics->getBody()->SetGravityScale(1);
                 physics->getBody()->SetLinearDamping(0.0f);
             }
@@ -56,12 +56,35 @@ void BraveAdventurerMovement::go(sf::Time frameTime) {
                 nextState = MoveState::onLadder;
             break;
         case MoveState::onGround:
-            if(input->walkLeft && velocity.x > -maxGroundSpeed) //Move left
+            body->ApplyForceToCenter(b2Vec2(-25*velocity.x,0),true); //Slow x movement
+            if(input->walkLeft) //Move left
+                nextState=MoveState::leftWalk;
+            if(input->walkRight) //Move right
+                nextState=MoveState::rightWalk;
+            if(!physics->onGround()) //in air!
+                nextState = MoveState::inAir;
+            if(input->jump) //Jump
+                nextState = MoveState::jumping;
+            if((input->climbUp || input->climbDown) && physics->overLadder()) //Start climbing on a ladder
+                nextState = MoveState::onLadder;
+            break;
+        case MoveState::leftWalk:
+            if(input->walkLeft && velocity.x > -maxGroundSpeed) //Contimue to walk left
                 body->ApplyForceToCenter(b2Vec2(-100.0f-velocity.x*100.0f/maxGroundSpeed,0),true);
-            if(input->walkRight && velocity.x < maxGroundSpeed) //Move right
+            if(!input->walkLeft)
+                nextState= MoveState::onGround;
+            if(!physics->onGround()) //in air!
+                nextState = MoveState::inAir;
+            if(input->jump) //Jump
+                nextState = MoveState::jumping;
+            if((input->climbUp || input->climbDown) && physics->overLadder()) //Start climbing on a ladder
+                nextState = MoveState::onLadder;
+            break;
+        case MoveState::rightWalk:
+            if(input->walkRight && velocity.x < maxGroundSpeed) //Contimue to walk left
                 body->ApplyForceToCenter(b2Vec2(100.0f-velocity.x*100.0f/maxGroundSpeed,0),true);
-            if((!input->walkLeft && !input->walkRight) || (input->walkLeft && input->walkRight)) //No movement, slow him
-                body->ApplyForceToCenter(b2Vec2(-25*velocity.x,0),true);
+            if(!input->walkRight)
+                nextState= MoveState::onGround;
             if(!physics->onGround()) //in air!
                 nextState = MoveState::inAir;
             if(input->jump) //Jump
@@ -78,19 +101,59 @@ void BraveAdventurerMovement::go(sf::Time frameTime) {
             break;
         case MoveState::onLadder:
             if(input->climbUp) //climb up
-                body->SetLinearVelocity(b2Vec2(body->GetLinearVelocity().x, climbSpeed));
+                nextState=MoveState::ladderUp;
             if(input->climbDown) //climb down
-                body->SetLinearVelocity(b2Vec2(body->GetLinearVelocity().x, -climbSpeed));
+                nextState=MoveState::ladderDown;
             if(input->jump) //jump off
                 nextState = MoveState::jumping;
             if(!physics->overLadder()) //off Ladder
                 nextState = MoveState::inAir;
             if(velocity.x > -maxLadderSpeed && velocity.x < maxLadderSpeed) { //Movement
                 if(input->walkLeft)
-                    body->ApplyForceToCenter(b2Vec2(-100.0f-velocity.x*100.0f/maxLadderSpeed,0),true);
+                    nextState=MoveState::ladderLeft;
                 if(input->walkRight)
-                    body->ApplyForceToCenter(b2Vec2(100.0f-velocity.x*100.0f/maxLadderSpeed,0),true);
+                    nextState=MoveState::ladderRight;
             }
+            break;
+        case MoveState::ladderLeft:
+            if(input->walkLeft && velocity.x >= -maxLadderSpeed) //Moving
+                body->ApplyForceToCenter(b2Vec2(-100.0f-velocity.x*100.0f/maxLadderSpeed,0),true);
+            else
+                nextState=MoveState::onLadder;
+            if(!physics->overLadder()) //No longer on ladder
+                nextState=MoveState::inAir;
+            if(input->jump) //jump off
+                nextState = MoveState::jumping;
+            break;
+        case MoveState::ladderRight:
+            if(input->walkRight && velocity.x <= maxLadderSpeed) //Moving
+                body->ApplyForceToCenter(b2Vec2(100.0f-velocity.x*100.0f/maxLadderSpeed,0),true);
+            else
+                nextState=MoveState::onLadder;
+            if(!physics->overLadder()) //No longer on ladder
+                nextState=MoveState::inAir;
+            if(input->jump) //jump off
+                nextState = MoveState::jumping;
+            break;
+        case MoveState::ladderUp:
+            if(input->climbUp && velocity.y <= maxLadderSpeed) //Moving
+                body->ApplyForceToCenter(b2Vec2(0,100.0f-velocity.x*100.0f/maxLadderSpeed),true);
+            else
+                nextState=MoveState::onLadder;
+            if(!physics->overLadder()) //No longer on ladder
+                nextState=MoveState::inAir;
+            if(input->jump) //jump off
+                nextState = MoveState::jumping;
+            break;
+        case MoveState::ladderDown:
+            if(input->climbDown && velocity.y >= -maxLadderSpeed) //Moving
+                body->ApplyForceToCenter(b2Vec2(0,-100.0f-velocity.x*100.0f/maxLadderSpeed),true);
+            else
+                nextState=MoveState::onLadder;
+            if(!physics->overLadder()) //No longer on ladder
+                nextState=MoveState::inAir;
+            if(input->jump) //jump off
+                nextState = MoveState::jumping;
             break;
         }
     }
